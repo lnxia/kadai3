ad2fe3b0a8f19f77bdfbb13280a67862dbdd6041
lnxia <xialina1988@icloud.com>
2017-06-01 20:27:39 +0900

    My first code for kadai3

---
+++ 16745201.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+# 16745201.sh:
+
+
+ARGS=2 
+E_BADARGS=65
+
+if [ $# -ne "$ARGS" ]
+then
+echo "Usage: `basename $0` first-number second-number" 
+exit $E_BADARGS 
+fi
+
+
+function  gcd()
+{
+
+ dividend=$1
+ divisor=$2
+
+ remainder=1
+
+until [ "$remainder" -eq 0 ]
+do
+ let "remainder = $dividend % $divisor"
+ dividend=$divisor 
+ divisor=$remainder
+done
+
+}
+gcd $1 $2
+
+echo; echo "GCD of $1 and $2 = $dividend"; echo
+ 
+
+
